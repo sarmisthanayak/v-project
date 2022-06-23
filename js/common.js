5d0791544cb16354f72ad5eeb794b9e44197a714
sarmisthanayak <108008849+sarmisthanayak@users.noreply.github.com>
2022-06-23 22:24:41 +0530

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,14 +2,11 @@ let timVine = document.getElementById("tim-vine");
 let navbar = document.getElementById("main-nav");
 
 let navPos = navbar.getBoundingClientRect().top;
-
 window.addEventListener("scroll", e => {
   let scrollPos = window.scrollY;
   if (scrollPos > navPos) {
     navbar.classList.add('sticky');
-    header.classList.add('navbarOffsetMargin');
   } else {
     navbar.classList.remove('sticky');
-    header.classList.remove('navbarOffsetMargin');
   }
 });
